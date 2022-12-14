0b3e78715fd5648158b4cad521cb8b42c15b2b40
Patrick Frantzen <patrick.frantzen@web.de>
2022-12-14 13:50:29 +0100

    header implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['code.jpg', 'laptop.jpg', 'university.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage= true;
-      },10)
-    }, 8000);
-  }
+export class AppComponent {
 
 
 }
